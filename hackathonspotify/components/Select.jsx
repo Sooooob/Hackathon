7ad5445ec53776bc1aeb3aae4247b0a9f7bea721
Kurtis <kurtis.hardy@wedo-digital.co.uk>
2023-04-20 15:57:19 +0100

    Colouring in

--- hackathonspotify/components/Select.jsx
+++ hackathonspotify/components/Select.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
+import classnames from "classnames";
 
 const Select = ({ data, selected, onChange, label }) => {
     return (
@@ -8,10 +9,14 @@ const Select = ({ data, selected, onChange, label }) => {
                 <>
                     <div className="relative mt-2">
                         <Listbox.Button
-                            className="relative flex items-center justify-between w-full cursor-default rounded-md bg-gray-900 text-white pl-3 pr-4 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            className={classnames(
+                                "relative cursor-pointer flex items-center justify-between w-full cursor-default rounded-md bg-black text-white pl-3 pr-4 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600 sm:text-sm sm:leading-6",
+                                open && "ring-green-600"
+                            )}
+                        >
                             {selected?.name
-                                ? <span className="bg-gray-900 text-white">{selected?.name}</span>
-                                : <span className="bg-gray-900 text-gray-300">{label || "..."}</span>
+                                ? <span className="bg-black text-white">{selected?.name}</span>
+                                : <span className="bg-black text-gray-300">{label || "..."}</span>
                             }
                             <span className="material-icons-outlined !text-3xl hover:text-gray-500 transition-all">{open ? "arrow_drop_up" : "arrow_drop_down"}</span>
                         </Listbox.Button>
@@ -24,11 +29,11 @@ const Select = ({ data, selected, onChange, label }) => {
                             leaveTo="opacity-0"
                         >
                             <Listbox.Options
-                                className="absolute border z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-gray-900 text-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                className="absolute mb-4 border z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-black text-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                                 {data.map((item) => (
                                     <Listbox.Option
                                         key={item.id}
-                                        className="text-gray-900 border-t relative cursor-pointer hover:bg-gray-600 select-none py-2 pl-3 pr-9"
+                                        className="text-black border-t relative cursor-pointer hover:bg-gray-600 select-none py-2 pl-3 pr-9"
                                         value={item}
                                     >
                                         {({ selected, active }) => (
