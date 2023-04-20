73b7dcd1805f38d96db7b570f2fe6d29b2bfef52
Kurtis <kurtis.hardy@wedo-digital.co.uk>
2023-04-20 09:52:22 +0100

    Mood progress

--- hackathonspotify/pages/mood.tsx
+++ hackathonspotify/pages/mood.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 import Emo from '../public/emo.png'
-
+import { useEffect } from "react";
 
 const SpotifyAlbums = [
     { id: 1, domainantColour: '#000000', },
@@ -25,10 +25,44 @@ const SpotifyAlbums = [
     { id: 19, domainantColour: '#000000', },
     { id: 20, domainantColour: '#000000', },
     { id: 21, domainantColour: '#000000', },
-]
+    { id: 2, domainantColour: '#000000', },
+    { id: 3, domainantColour: '#000000', },
+    { id: 4, domainantColour: '#000000', },
+    { id: 5, domainantColour: '#000000', },
+    { id: 6, domainantColour: '#000000', },
+    { id: 7, domainantColour: '#000000', },
+    { id: 8, domainantColour: '#000000', },
+    { id: 9, domainantColour: '#000000', },
+    { id: 10, domainantColour: '#000000', },
+    { id: 11, domainantColour: '#000000', },
+    { id: 12, domainantColour: '#000000', },
+    { id: 13, domainantColour: '#000000', },
+    { id: 14, domainantColour: '#000000', },
+    { id: 15, domainantColour: '#000000', },
+    { id: 16, domainantColour: '#000000', },
+    { id: 17, domainantColour: '#000000', },
+    { id: 18, domainantColour: '#000000', },
+    { id: 19, domainantColour: '#000000', },
+    { id: 20, domainantColour: '#000000', },
+    { id: 21, domainantColour: '#000000', },
+    { id: 2, domainantColour: '#000000', },
+    { id: 3, domainantColour: '#000000', },
+    { id: 4, domainantColour: '#000000', },
+    { id: 5, domainantColour: '#000000', },
+    { id: 6, domainantColour: '#000000', },
+    { id: 7, domainantColour: '#000000', },
+    { id: 8, domainantColour: '#000000', },
+    { id: 9, domainantColour: '#000000', },
+    { id: 10, domainantColour: '#000000', },
+];
+
 export default function Mood() {
+    useEffect(() => {
+
+    }, []);
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between py-16 px-24">
+        <main className="flex min-h-screen flex-col items-center justify-between py-16 px-8 lg:px24 transition-all">
             <div className="flex w-full justify-between items-center">
                     <span className="material-icons-outlined !text-4xl hover:text-gray-500 transition-all">timer</span>
 
@@ -54,11 +88,12 @@ export default function Mood() {
 
             <div className="py-8">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                    <div className="mx-auto max-w-2xl lg:max-w-none mt-8 grid grid-cols-1 gap-0.5 overflow-hidden text-center sm:grid-cols-2 lg:grid-cols-6">
-                        {SpotifyAlbums.map((album) => (
+                    <div className="mx-auto max-w-2xl lg:max-w-none mt-8 grid grid-cols-1 sm:grid-cols-2 md:grip-col-4 lg:grid-cols-6 overflow-hidden text-center ">
+                        {SpotifyAlbums.map((album, index) => (
                             <div
                                 key={album.id}
-                                className="flex justify-center items-center w-40 h-40 bg-white/5 p-8 border transform transition duration-500 ease-in-out hover:origin-top"
+                                className="flex justify-center items-center w-40 h-40 bg-white/5 p-8 border transform transition duration-500 ease-in-out card"
+                                style={{ animationDelay: `${((index+1) * 100) / 2}ms`, }}
                             >
                                 here
                             </div>
