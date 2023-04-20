099d923931653a26f2a0299c8ed9e8c54d9f6972
Kurtis <kurtis.hardy@wedo-digital.co.uk>
2023-04-20 08:55:43 +0100

    Moodify progress

--- hackathonspotify/pages/mood.tsx
+++ hackathonspotify/pages/mood.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import Emo from '../public/emo.png'
+
 
 const SpotifyAlbums = [
     { id: 1, domainantColour: '#000000', },
@@ -15,22 +15,29 @@ const SpotifyAlbums = [
     { id: 9, domainantColour: '#000000', },
     { id: 10, domainantColour: '#000000', },
     { id: 11, domainantColour: '#000000', },
-    { id: 2, domainantColour: '#000000', },
-    { id: 3, domainantColour: '#000000', },
-    { id: 4, domainantColour: '#000000', },
-    { id: 5, domainantColour: '#000000', },
-    { id: 6, domainantColour: '#000000', },
-    { id: 7, domainantColour: '#000000', },
-    { id: 8, domainantColour: '#000000', },
-    { id: 9, domainantColour: '#000000', },
-    { id: 10, domainantColour: '#000000', },
-    { id: 11, domainantColour: '#000000', },
+    { id: 12, domainantColour: '#000000', },
+    { id: 13, domainantColour: '#000000', },
+    { id: 14, domainantColour: '#000000', },
+    { id: 15, domainantColour: '#000000', },
+    { id: 16, domainantColour: '#000000', },
+    { id: 17, domainantColour: '#000000', },
+    { id: 18, domainantColour: '#000000', },
+    { id: 19, domainantColour: '#000000', },
+    { id: 20, domainantColour: '#000000', },
+    { id: 21, domainantColour: '#000000', },
 ]
 export default function Mood() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between py-16 px-24">
-            <div></div>
-            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Moodify</h2>
+            <h2 className="flex text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                M
+                <Image
+                src={Emo}
+                alt="o"
+                className="px-1 pt-4"
+                width={50}
+                height={14}
+            />odify</h2>
             <div className="py-8">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:max-w-none mt-8 grid grid-cols-1 gap-0.5 overflow-hidden text-center sm:grid-cols-2 lg:grid-cols-6">
