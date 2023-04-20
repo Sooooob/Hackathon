bbb1e3a257ba8a2481d39a74d1047fc2ba6025b1
Kurtis <kurtis.hardy@wedo-digital.co.uk>
2023-04-20 09:15:24 +0100

    Adding icons

--- hackathonspotify/pages/_app.tsx
+++ hackathonspotify/pages/_app.tsx
@@ -3,6 +3,8 @@ import '~/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
 import { AppProps } from 'next/app'
 
+import 'material-icons/iconfont/material-icons.css'
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
--- hackathonspotify/pages/mood.tsx
+++ hackathonspotify/pages/mood.tsx
@@ -29,15 +29,33 @@ const SpotifyAlbums = [
 export default function Mood() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between py-16 px-24">
-            <h2 className="flex text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                M
-                <Image
-                src={Emo}
-                alt="o"
-                className="px-1 pt-4"
-                width={50}
-                height={14}
-            />odify</h2>
+            <div className="flex w-full justify-between items-center">
+                    <span className="material-icons-outlined !text-4xl hover:text-gray-500 transition-all">timer</span>
+
+                <h2 className="flex flex-grow items-center justify-center text-center text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                    M
+                    <Image
+                        src={Emo}
+                        alt="o"
+                        className="px-1 pt-4"
+                        width={50}
+                        height={14}
+                    />odify</h2>
+
+                <div className="flex gap-3 items-center">
+                    <button className="border rounded-full w-12 h-12 hover:text-gray-500 transition-all">
+                        <span className="material-icons-outlined !text-3xl">question_mark</span>
+                    </button>
+                    <button className="border rounded-full w-12 h-12 hover:text-gray-500 transition-all">
+                        <span className="material-icons-outlined !text-2xl">menu</span>
+                    </button>
+                </div>
+            </div>
+
+            <div class="w-40 h-40 bg-blue-500 hover:bg-red-500 transform transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110">
+                <p class="text-white text-center font-bold pt-16">Hover Me!</p>
+            </div>
+
             <div className="py-8">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:max-w-none mt-8 grid grid-cols-1 gap-0.5 overflow-hidden text-center sm:grid-cols-2 lg:grid-cols-6">
