d587b4b44329fe175921f55df894496688093775
Sebastian Barnes <seb@awsm.tech>
2023-04-20 08:36:45 +0100

    Next auth with spotify

--- hackathonspotify/pages/index.tsx
+++ hackathonspotify/pages/index.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useSession } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useSession({required: true, });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
