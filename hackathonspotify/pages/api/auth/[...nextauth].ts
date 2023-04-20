1f8289e56a4d1fe2b55569b76b30ca741355b8c3
Sebastian Barnes <seb@awsm.tech>
2023-04-20 10:49:25 +0100

    Installing mat icons

--- hackathonspotify/pages/api/auth/[...nextauth].ts
+++ hackathonspotify/pages/api/auth/[...nextauth].ts
@@ -3,9 +3,34 @@ import SpotifyProvider from "next-auth/providers/spotify"
 
 export default NextAuth({
     providers: [
-      SpotifyProvider({
-        clientId: process.env.CLIENT_ID ?? "",
-        clientSecret: process.env.CLIENT_SECRET ?? "",
-      }),
+        SpotifyProvider({
+            clientId: process.env.CLIENT_ID ?? "",
+            clientSecret: process.env.CLIENT_SECRET ?? "",
+        }),
     ],
-  })
+
+    callbacks: {
+        async session({ session, user, token }) {
+            // @ts-ignore
+            session.accessToken = token.accessToken;
+            return session
+        },
+        async redirect({ url, baseUrl }) {
+            // Allows relative callback URLs
+            if (url.startsWith("/"))
+                return `${baseUrl}${url}`
+
+            // Allows callback URLs on the same origin
+            else if (new URL(url).origin === baseUrl)
+                return url
+            return baseUrl
+        },
+        async jwt({ token, account }) {
+            // Persist the OAuth access_token to the token right after signin
+            if (account) {
+                token.accessToken = account.access_token
+            }
+            return token
+        }
+    }
+})
